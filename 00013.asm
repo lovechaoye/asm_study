20cce1c0bbe9ae8388989e737aaaf4e90abae8df
lichao <w3c@shouce.ren>
2019-01-14 14:16:43 +0800

    modi 14 a3

--- 00013.asm
+++ 00013.asm
@@ -1,24 +1,24 @@
 include io32.inc
 .data
-bvar byte 
+bvar byte 12H
 wvar word 1234h
 dvar dword 12345678h
 
 .code
 
 start:
-mov [ebx],255
+;mov [ebx],255 两个操作数均没有明确类型,错误
 mov byte ptr [ebx],255
 mov word ptr [ebx],255
 mov dword ptr [ebx],255
-mov [ebx+4],200
-mov byte ptr[ebx+4]
+;mov [ebx+4],200 两个操作数均没有明确类型,错误
+mov byte ptr[ebx+4],255
 mov wvar[esi],200
-mov dwar[edi*4],200
+mov dvar[edi*4],200
 
-mov ds,@data
-mov ax,@data
-moc ds,ax
+;mov ds,@data
+;mov ax,@DATA 使用这个符号的变量会报错误,符号冲突,目前不知道怎么解决
+;mov ds,ax
 nop;等价于 xchg eax,eax
 
 
--- 00014.asm
+++ 00014.asm
@@ -1,1 +1,11 @@
-;hello monday
+;²âÊÔ´ø@µÄÖµ
+include io32.inc
+.data 
+	bvar byte 1
+.code
+start:
+	mov ax,@data
+	call disprw
+exit 0
+
+end start
